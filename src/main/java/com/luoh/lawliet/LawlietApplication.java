70176785271155ce3d6ea5fc19aa99f9834c2872
luoheng <luohenghlx@163.com>
2019-07-16 22:30:44 +0800

    Initial commit

---
+++ src/main/java/com/luoh/lawliet/LawlietApplication.java
@@ -0,0 +1,13 @@
+package com.luoh.lawliet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawlietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawlietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luoh/lawliet/LawlietApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luoh.lawliet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LawlietApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
