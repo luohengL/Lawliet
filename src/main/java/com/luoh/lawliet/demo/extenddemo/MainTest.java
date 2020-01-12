2fb5512f19dd8479bdbcb798eda4cf5f92afe81e
luoheng <luohenghlx@163.com>
2020-01-12 19:59:27 +0800

    add pythonTxt

---
+++ src/main/java/com/luoh/lawliet/demo/extenddemo/MainTest.java
@@ -0,0 +1,9 @@
+package com.luoh.lawliet.demo.extenddemo;
+
+/**
+ * @autoor luoh
+ * @Date 2019-07-31
+ * @desciption:
+ **/
+public class MainTest {
+}
---
+++ src/main/java/com/luoh/lawliet/demo/extenddemo/Person.java
@@ -0,0 +1,22 @@
+package com.luoh.lawliet.demo.extenddemo;
+
+public class Person {
+   private String name;
+   private String hoby;
+    private String sex;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHoby() {
+        return hoby;
+    }
+
+    public void setHoby(String hoby) {
+        this.hoby = hoby;
+    }
+}
